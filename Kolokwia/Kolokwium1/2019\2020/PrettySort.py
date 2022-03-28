b5e62e784e421e466e3be0a5679052f3e2bc4a85
Kacper Korta <101141624+kkorta@users.noreply.github.com>
2022-03-28 21:41:27 +0200

    Add files via upload

---
+++ Kolokwia/Kolokwium1/2019\2020/PrettySort.py
@@ -0,0 +1,65 @@
+def convert_to_pretty(x):
+    count = [0] * 10
+    num = x
+    while num != 0:
+        count[num % 10] += 1
+        num //= 10
+
+    single = 0
+    many = 0
+    for i in range(10):
+        if count[i] == 1:
+            single += 1
+        elif count[i] > 1:
+            many += 1
+
+    return x, single, many
+
+
+def counting_sort(T, index, n):
+    C = [0] * 10
+    B = [0] * n
+
+    for x in T:
+        C[x[index]] += 1
+
+    for i in range(1, 10):
+        C[i] = C[i] + C[i - 1]
+
+    for i in range(n - 1, -1, -1):
+        B[C[T[i][index]] - 1] = T[i]
+        C[T[i][index]] -= 1
+
+
+    if index == 2:
+        counter = 0
+        for i in range(n - 1, -1, -1):
+            T[counter] = B[i]
+            counter += 1
+    else:
+        counter = n - 1
+        for i in range(n):
+            T[i] = B[counter][0]
+            counter -= 1
+
+
+    return T
+
+def pretty_sort(T):
+    n = len(T)
+    for i in range(n):
+        T[i] = convert_to_pretty(T[i])
+
+    counting_sort(T, 2, n)
+    counting_sort(T, 1, n)
+
+
+
+    return T
+
+
+
+
+T = [123, 455, 2344, 2145]
+T2 = [11111, 245, 566, 7889, 1224455, 99883361, 5]
+print(pretty_sort(T2))
