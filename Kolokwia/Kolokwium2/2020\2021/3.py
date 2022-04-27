0805309a3f5a3fcb9357bac046382656bbf61ba5
Kacper Korta <101141624+kkorta@users.noreply.github.com>
2022-04-27 12:36:24 +0200

    fixed

--- Kolokwia/Kolokwium2/2020\2021/3.py
+++ Kolokwia/Kolokwium2/2020\2021/3.py
@@ -1,5 +1,6 @@
-from math import inf
 
+from pprint import pprint
+from queue import PriorityQueue
 
 def count(T, visited, row, col, size):
     if row < 0 or row >= len(T) or col < 0 or col >= len(T) or T[row][col] == 0 or visited[row][col] == 1:
@@ -17,66 +18,41 @@ def count(T, visited, row, col, size):
 
 def collect_oil(T, n):
     m = len(T)
-    total_oil = 0
     visited = [[-1 for _ in range(n)] for _ in range(m)]
 
     for i in range(n):
         if T[0][i] != 0 and visited[0][i] == -1:
             T[0][i] = count(T, visited, 0, i, 0)
-            total_oil += T[0][i]
 
         elif T[0][i] != 0 and visited[0][i] == 1:
             T[0][i] = 0
 
-    return T, total_oil
+    return T
 
 
 def plan(T):
-    n = len(T[0])
-    T, x = collect_oil(T, n)
-    tab = [[inf for _ in range(x + 1)] for _ in range(n)]
-    if x == 1:
-        return [0]
 
-    tab[0][T[0][0]] = 1
-
-    for i in range(1, n):
-        for j in range(x + 1):
-            if j + 1 < x + 1:
-                tab[i][j] = min(tab[i][j], tab[i - 1][j + 1])
-                if j + T[0][i] < x + 1 and tab[i - 1][j + 1] != inf:
-                    tab[i][j + T[0][i]] = min(tab[i][j + T[0][i]], tab[i][j] + 1)
-
-    index = 0
-    minim = inf
-    for i in range(x + 1):
-        if tab[n - 1][i] <= minim and tab[n - 1][i] != inf:
-            minim = tab[n - 1][i]
-            index = i
-
-
-    return get_solution(T, tab, index, n, x + 1)
-
-def get_solution(T, tab, index, n, s):
-    height = n - 1
     solution = []
-    while tab[height][index] != inf and height > 0 and index >= 0:
-        if index + 1 < s:
-            if tab[height][index] == tab[height - 1][index + 1]:
-                height -= 1
-                index += 1
-
-        if T[0][height] != 0 and index - T[0][height] >= 0:
-            if tab[height][index] == tab[height][index - T[0][height]] + 1:
-                solution.append(height)
-                index -= T[0][height]
-
-
+    n = len(T[0])
+    T = collect_oil(T, n)
+    fuel = T[0][0]
     solution.append(0)
-    solution.reverse()
-    return solution
+    q = PriorityQueue()
+    i = 1
+    while fuel < n and i < n:
+        if i <= fuel and T[i] != 0:
+            q.put((-T[0][i], i))
 
-from pprint import pprint
+        if fuel < i:
+            val, pos = q.get()
+            fuel -= val
+            if T[0][i] != 0:
+                q.put((-T[0][i], i))
+            solution.append(pos)
+
+        i += 1
+
+    return sorted(solution)
 
 T0 = [
     [1, 0],
