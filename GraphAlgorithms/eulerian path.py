2fe22dd5cf5a7554d4ad3e166c59c786ee881412
Kacper Korta <101141624+kkorta@users.noreply.github.com>
2022-05-23 19:38:19 +0200

    Update eulerian path.py

--- GraphAlgorithms/eulerian path.py
+++ GraphAlgorithms/eulerian path.py
@@ -1,16 +1,18 @@
-def dfs(g, n, v, path):
-    for e in range(n):
+def dfs(g, n, v, path, last):
+    for e in range(last[v], n):
         if g[v][e] != 0:
             g[v][e] = 0
             g[e][v] = 0
-            dfs(g, n, e, path)
+            last[v] = e + 1
+            dfs(g, n, e, path, last)
 
     path.insert(0, v)
 
 def eulerian_path(g):
     n = len(g)
     path = []
-    dfs(g, n, 0, path)
+    last = [0] * n
+    dfs(g, n, 1, path, last)
     return path
 
 T = [[0, 1, 1, 0, 0, 0],
@@ -20,4 +22,3 @@ T = [[0, 1, 1, 0, 0, 0],
      [0, 0, 0, 1, 0, 1],
      [0, 1, 1, 1, 1, 0]]
 
-print(eulerian_path(T))
